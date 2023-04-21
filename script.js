6b505bff546a9f84787c066efc0e82d315bf2fee
FHS Clock <101165376+fhsclock@users.noreply.github.com>
2023-04-21 14:06:44 -0500

    Add files via upload
    updated. fix synchronizing error. 04/21/23

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 Copright 2023 Riley Jones. 
 All Rights Reserved.
 
-JavaScript of FHS Clock.
+JS of FHS Clock.
 
 * * * * * * * * * * * * * * */
 
@@ -12,8 +12,8 @@ function weekday() {
 
     function t() {
         var e = o();
-        return setInterval(function() {
-            r(a(), e)
+        return setInterval(function () {
+            s(a(), e)
         }, 1e3)
     }
 
@@ -26,28 +26,29 @@ function weekday() {
         return e.setHours(c[0]), e.setMinutes(c[1]), e.setSeconds(c[2]), e
     }
 
-    function r(e, t) {
-        var a = t.getTime() - e.getTime(),
+    function s(e, t) {
+        var a = t.getTime() - e.getTime() + 1e3, // synchronizing fix
             o = Math.floor((d = a >= 0 ? a : a + 864e5) / 3600 / 1e3),
-            r = Math.floor(d / 60 / 1e3) - 60 * o;
-        u(o, r, Math.floor(d / 1e3) - 3600 * o - 60 * r)
+            s = Math.floor(d / 60 / 1e3) - 60 * o;
+        u(o, s, Math.floor(d / 1e3) - 3600 * o - 60 * s)
     }
 
-    function s(e, t) {
+    function r(e, t) {
         return e + " "
     }
 
     function u(e, t, a) {
-        var o, r, s;
-        $("#countdown").html(e + "h " + t + "m " + a + "s ")
+        $("#countdown")
+            .html(e + "h " + t + "m " + a + "s ")
     }
-    if (0 == e.getDay() && document.getElementById("schoolhours").classList.add("hide"), 1 == e.getDay()) {
+    if (0 == e.getDay() && document.getElementById("schoolhours")
+        .classList.add("hide"), 1 == e.getDay()) {
         var c = [14, 36, 0];
 
         function t() {
             var e = o();
-            return setInterval(function() {
-                r(a(), e)
+            return setInterval(function () {
+                s(a(), e)
             }, 1e3)
         }
 
@@ -60,19 +61,20 @@ function weekday() {
             return e.setHours(c[0]), e.setMinutes(c[1]), e.setSeconds(c[2]), e
         }
 
-        function r(e, t) {
-            var a = t.getTime() - e.getTime(),
+        function s(e, t) {
+            var a = t.getTime() - e.getTime() + 1e3, // synchronizing fix
                 o = Math.floor((d = a >= 0 ? a : a + 864e5) / 3600 / 1e3),
-                r = Math.floor(d / 60 / 1e3) - 60 * o;
-            u(o, r, Math.floor(d / 1e3) - 3600 * o - 60 * r)
+                s = Math.floor(d / 60 / 1e3) - 60 * o;
+            u(o, s, Math.floor(d / 1e3) - 3600 * o - 60 * s)
         }
 
-        function s(e, t) {
+        function r(e, t) {
             return e + " "
         }
 
         function u(e, t, a) {
-            $("#countdown").html(e + "h " + t + "m " + a + "s ")
+            $("#countdown")
+                .html(e + "h " + t + "m " + a + "s ")
         }
         t()
     }
@@ -84,27 +86,39 @@ function updateClock() {
         t = e.getHours(),
         a = e.getHours(),
         o = e.getMinutes(),
-        r = e.getSeconds(),
-        s = (t = 0 == (t = a > 12 ? a - 12 : a) ? 12 : t) + ":" + (o = (o < 10 ? "0" : "") + o) + ":" + (r = (r < 10 ? "0" : "") + r) + " " + (a < 12 ? "AM" : "PM");
-    $("#clock").html(s)
+        s = e.getSeconds(),
+        r = (t = 0 == (t = a > 12 ? a - 12 : a) ? 12 : t) + ":" + (o = (o < 10 ? "0" : "") + o) + ":" + (s = (s < 10 ? "0" : "") + s) + " " + (a < 12 ? "AM" : "PM");
+    $("#clock")
+        .html(r)
 }
-weekday(), $(document).ready(function() {
-    setInterval(updateClock, 1e3)
-});
+weekday(), $(document)
+    .ready(function () {
+        setInterval(updateClock, 1e3)
+    });
 var weekday, d = new Date;
 (weekday = Array(7))[0] = "Sunday", weekday[1] = "Monday", weekday[2] = "Tuesday", weekday[3] = "Wednesday", weekday[4] = "Thursday", weekday[5] = "Friday", weekday[6] = "Saturday";
 var n = weekday[d.getDay()];
-document.getElementById("day").innerHTML = n;
+document.getElementById("day")
+    .innerHTML = n;
 var d2 = new Date,
     weekday2 = Array(7);
 weekday2[0] = "N/A", weekday2[1] = "2:36 PM", weekday2[2] = "2:36 PM", weekday2[3] = "2:36 PM", weekday2[4] = "2:00 PM", weekday2[5] = "2:36 PM", weekday2[6] = "N/A";
 var n2 = weekday2[d2.getDay()];
-document.getElementById("release").innerHTML = n2, $(document).ready(function() {
-    var e = new Date,
-        t = new Date,
-        a = (new Date).getTime();
-    a > e.setHours(7, 18) && a < t.setHours(14, 36) ? (document.getElementById("schoolhours").classList.add("open"), document.getElementById("releasetime").classList.add("open")) : (document.getElementById("schoolhours").classList.add("closed"), document.getElementById("releasetime").classList.add("closed"), document.getElementById("schoolend").classList.add("show"), document.getElementById("releaseend").classList.add("show"))
-});
+document.getElementById("release")
+    .innerHTML = n2, $(document)
+        .ready(function () {
+            var e = new Date,
+                t = new Date,
+                a = (new Date)
+                    .getTime();
+            a > e.setHours(7, 18) && a < t.setHours(14, 36) ? (document.getElementById("schoolhours")
+                .classList.add("open"), document.getElementById("releasetime")
+                    .classList.add("open")) : (document.getElementById("schoolhours")
+                        .classList.add("closed"), document.getElementById("releasetime")
+                            .classList.add("closed"), document.getElementById("schoolend")
+                                .classList.add("show"), document.getElementById("releaseend")
+                                    .classList.add("show"))
+        });
 var text = ["N/A"],
     counter = 0,
     elem = document.getElementById("countdown"),
